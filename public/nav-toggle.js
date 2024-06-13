58e75f2d16d5c0fa8812795df20d2b3e84226bbe
Harper Foley <harper.e.foley@gmail.com>
2024-06-12 21:09:19 -0700

    Adds toggle to mobile menu

---
+++ public/nav-toggle.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    let navbarCollapse = document.getElementById('navbar-collapse-with-animation');
+    navbarCollapse.classList.toggle('hidden');
+});
+
